41ded40b5cb2f8fe283d5cd5406b2a54f0ed88da
Eduardo Chumpitaz Tome <chumpitome@gmail.com>
2023-06-26 01:01:28 -0500

    Upload

--- contracts/PublicSale.sol
+++ contracts/PublicSale.sol
@@ -30,6 +30,11 @@ contract PublicSale is
     // Crear su setter
     address gnosisSafeWallet;
 
+    mapping(uint256 => bool) public soldNFT;
+
+    uint256[] availableNFT;
+    uint256 public numberAvailableNFT;
+
     event DeliverNft(address winnerAccount, uint256 nftId);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -57,24 +62,42 @@ contract PublicSale is
         //         * Mensaje de error: "Public Sale: Not enough token balance"
         // 4 - el _id se encuentre entre 1 y 30
         //         * Mensaje de error: "NFT: Token id out of range"
-
+        require(!soldNFT[_id],"Public Sale: id not available");
+        require(_id > 0 && _id < 31,"NFT: Token id out of range");
         // Obtener el precio segun el id
         uint256 priceNft = _getPriceById(_id);
+
+        require(miPrimerToken.allowance(msg.sender, address(this)) >= priceNft,"Public Sale: Not enough allowance");
+
+        require(miPrimerToken.balanceOf(msg.sender) >= priceNft,"Public Sale: Not enough token balance");
+
+        updateStatusToken(_id);
 
         // Purchase fees
         // 10% para Gnosis Safe (fee)
         // 90% se quedan en este contrato (net)
         // from: msg.sender - to: gnosisSafeWallet - amount: fee
         // from: msg.sender - to: address(this) - amount: net
+        uint256 fee = (priceNft * 10) / 100;
+        uint256 net = priceNft - fee;
+        miPrimerToken.transferFrom(msg.sender, gnosisSafeWallet, fee);
+        miPrimerToken.transferFrom(msg.sender, address(this), net);   
 
         // EMITIR EVENTO para que lo escuche OPEN ZEPPELIN DEFENDER
         emit DeliverNft(msg.sender, _id);
+    }
+
+    function updateStatusToken(uint256 _id) public {
+        soldNFT[_id] = true;
+        numberAvailableNFT--;
     }
 
     function depositEthForARandomNft() public payable {
         // Realizar 2 validaciones
         // 1 - que el msg.value sea mayor o igual a 0.01 ether
         // 2 - que haya NFTs disponibles para hacer el random
+        require(msg.value >= 0.01 ether, "Public Sale: Not enough ether");
+        require(numberAvailableNFT>0, "Public Sale: Not available NFTs");
 
         // Escgoer una id random de la lista de ids disponibles
         uint256 nftId = _getRandomNftId();
@@ -96,19 +119,27 @@ contract PublicSale is
 
     // PENDING
     // Crear el metodo receive
+    receive() external payable {
+        depositEthForARandomNft();
+    }
 
     ////////////////////////////////////////////////////////////////////////
     /////////                    Helper Methods                    /////////
     ////////////////////////////////////////////////////////////////////////
 
     // Devuelve un id random de NFT de una lista de ids disponibles
-    function _getRandomNftId() internal view returns (uint256) {}
+    function _getRandomNftId() internal view returns (uint256) {
+        uint256 idNFT = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 30;
+        return availableNFT[idNFT];
+
+    }
 
     // Según el id del NFT, devuelve el precio. Existen 3 grupos de precios
     function _getPriceById(uint256 _id) internal view returns (uint256) {
-        uint256 priceGroupOne;
-        uint256 priceGroupTwo;
-        uint256 priceGroupThree;
+        uint256 priceGroupOne = 500;
+        uint256 priceGroupTwo = _id * 1000;
+
+        uint256 priceGroupThree = 10000 * 10 ** 18 + (((block.timestamp - startDate) / 1 hours) * 1000 * 10 ** 18);
         if (_id > 0 && _id < 11) {
             return priceGroupOne;
         } else if (_id > 10 && _id < 21) {
